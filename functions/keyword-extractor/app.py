ae482d2daa2422dcc0ad4e516f059bef44887279
sichoi42 <42.4.sichoi@gmail.com>
2023-12-06 11:24:45 +0900

    [BE] FIX: Keyword Extractor 커넥션 중복 close 수정

--- functions/keyword-extractor/app.py
+++ functions/keyword-extractor/app.py
@@ -3,6 +3,7 @@ from query_services import save_extracted_words_to_database
 from query_services import get_new_products_from_database
 from db_config import create_connection
 
+
 def task_handler(event, context):
     try:
         # 데이터베이스 연결
@@ -11,7 +12,7 @@ def task_handler(event, context):
         product_list = get_new_products_from_database(connection)
 
         for product in product_list:
-            
+
             # 데이터베이스에서 product 카테고리의 title과 content 가져오기
             content_array = [product.product_content.split('\n')]
             title_array = [product.product_title.split('\n')]
@@ -27,20 +28,20 @@ def task_handler(event, context):
             wordrank_extractor = KRWordRank(min_count=2, max_length=10)
 
             # extract 메서드로 키워드 추출
-            keywords, rank, graph = wordrank_extractor.extract(texts, beta=0.85, max_iter=10)
+            keywords, rank, graph = wordrank_extractor.extract(
+                texts, beta=0.85, max_iter=10)
 
             # 추출된 키워드 중 상위 5개 선택
-            selected_keywords = [word for word, r in sorted(keywords.items(), key=lambda x: x[1], reverse=True)[:5]]
+            selected_keywords = [word for word, r in sorted(
+                keywords.items(), key=lambda x: x[1], reverse=True)[:5]]
 
             # 결과 출력
             print(f'\n추출된 키워드 수: \n{selected_keywords.count}\n')
 
             # 추출된 키워드를 데이터베이스에 저장
-            save_extracted_words_to_database(connection, product.product_id, selected_keywords)
+            save_extracted_words_to_database(
+                connection, product.product_id, selected_keywords)
 
-        # 데이터베이스 연결 종료
-        connection.close()
-        
     except Exception as e:
         print(e)
         connection.rollback()
